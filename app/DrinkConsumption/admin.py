7813f02d45141ed375d7e8c097a841a2894b0399
Lucas Maurice (Sonic) <lucas.maurice@outlook.com>
2021-07-04 22:39:21 -0400

    Added base model.

--- app/DrinkConsumption/admin.py
+++ app/DrinkConsumption/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
+
+admin.site.register(Container)
+admin.site.register(Product)
+admin.site.register(ProductContainer)
+admin.site.register(PersonnalContainer)
+admin.site.register(PersonnalTag)
+admin.site.register(PersonnalConsumption)
--- app/DrinkConsumption/models.py
+++ app/DrinkConsumption/models.py
@@ -1,1 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Container(models.Model):
+    name = models.CharField(max_length=200)
+    capacity = models.FloatField(help_text="Capacity of the container in Liters.")
+    def __str__(self):
+        return self.name + " (" + str(self.capacity) + " L)"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class ProductContainer(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    container = models.ForeignKey(Container, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.product.name + " - " + self.container.__str__()
+
+
+class PersonnalContainer(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    capacity = models.FloatField(help_text="Capacity of the container in Liters.")
+    cost = models.FloatField(default=0, help_text="Cost of a filling in CAD$")
+    def __str__(self):
+        return self.name + " (" + str(self.capacity) + " L)"
+
+
+class PersonnalTag(models.Model):
+    uid = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    linked_container = models.ForeignKey(PersonnalContainer, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.uid
+
+
+class PersonnalConsumption(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(PersonnalTag, on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductContainer, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username + " - " + self.product.product.name
