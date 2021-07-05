7c09e6c51aa7d99ecdea9f9b9b0f94490488d591
Lucas Maurice (Sonic) <lucas.maurice@outlook.com>
2021-07-04 20:49:14 -0400

    Remove Key.

--- app/AppManager/settings.py
+++ app/AppManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7$1f@qo0rjqbt4sigmn2o@wkvwxu*q$v7(+yy^^27xs@b!e0%0'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
