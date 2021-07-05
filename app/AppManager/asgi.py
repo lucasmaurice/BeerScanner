2bf2b10b86b2fec4660dccc0cd1cba3b95a9068d
Lucas Maurice (Sonic) <lucas.maurice@outlook.com>
2021-07-04 20:38:40 -0400

    Added some initial configs.

--- app/AppManager/asgi.py
+++ app/AppManager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BeerManager project.
+ASGI config for AppManager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeerManager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppManager.settings')
 
 application = get_asgi_application()
--- app/AppManager/wsgi.py
+++ app/AppManager/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BeerManager project.
+WSGI config for AppManager project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeerManager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppManager.settings')
 
 application = get_wsgi_application()
