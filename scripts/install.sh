e08a4ec037304b59282d9243039e3d8e449b3404
Lucas Maurice (Sonic) <sonic@justereseau.ca>
2021-08-03 19:38:49 +0100

    Added ssl config for NGINX.

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,3 +13,15 @@ pip install -r ../app/requirements.txt
 python3 ../app/manage.py makemigrations
 python3 ../app/manage.py migrate --noinput
 python3 ../app/manage.py runserver 0.0.0.0:8000
+
+# ACME
+export CF_Token="---"
+export CF_Account_ID="---"
+export CF_Zone_ID="---"
+~/.acme.sh/acme.sh --issue -d beer.justereseau.ca --dns dns_cf
+
+~/.acme.sh/acme.sh --install-cert -d beer.justereseau.ca \
+--cert-file /etc/nginx/certs/beer.justereseau.ca/cert \
+--key-file /etc/nginx/certs/beer.justereseau.ca/key \
+--fullchain-file /etc/nginx/certs/beer.justereseau.ca/fullchain \
+--reloadcmd "systemctl reload nginx.service"
